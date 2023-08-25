839dbc4180149b05622ec62f1e61debc962ef90d
lizord <mt.demakov@gmail.com>
2023-08-25 21:56:15 +0300

    ext2 module stub

---
+++ modules/ext2/main.c
@@ -0,0 +1,14 @@
+#include <kernel/mod/modules.h>
+#include <kernel/kernel.h>
+#include <kernel/util/log.h>
+
+K_STATUS load(){
+    return K_STATUS_OK;
+}
+
+K_STATUS unload(){
+    return K_STATUS_OK;
+}
+
+MODULE("ext2", &load, &unload)
+MODULE_DEPENDENCIES("pre:(dospart|gptpart)")
