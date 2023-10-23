a1f64cb4b248c6440d212556027418412b488b93
Lizord <mt.demakov@gmail.com>
2023-10-23 11:10:44 +0300

    beautiful ls

--- user/ls.c
+++ user/ls.c
@@ -1,16 +1,41 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+
+// This was made like this on purpose, for speed reasons, as I doubt,
+// that we will have really big inodes
+int inode_length(uint32_t ino) {
+    if (ino < 0) ino = (ino == INT_MIN) ? INT_MAX : -ino;
+    if (ino < 10) return 1;
+    if (ino < 100) return 2;
+    if (ino < 1000) return 3;
+    if (ino < 10000) return 4;
+    if (ino < 100000) return 5;
+    if (ino < 1000000) return 6;
+    if (ino < 10000000) return 7;
+    if (ino < 100000000) return 8;
+    if (ino < 1000000000) return 9;
+    /*      2147483647 is 2^31-1 - add more ifs as needed
+       and adjust this final return as well. */
+    return 10;
+}
 
 void dump(const char* path) {
 	DIR* root = opendir(path);
 	struct dirent* dirs = malloc(sizeof(struct dirent));
 	int n = 0;
 	int c = 1;
+
+	uint32_t longest_inode = 0;
+
 	if(root) {
 		struct dirent* dir;
 		while((dir = readdir(root))) {
+			if(dir->ino > longest_inode) {
+				longest_inode = dir->ino;
+			}
 			memcpy(&dirs[n], dir, sizeof(struct dirent));	
 			n++;
 			if(n >= c) {
@@ -38,8 +63,10 @@ void dump(const char* path) {
             break;
     }
 
+	int ino_width = inode_length(longest_inode);
+
 	for(int i = 0; i < n; i++) {
-		printf("%d %s\n", dirs[i].ino, dirs[i].name);
+		printf("%*d %s\n", ino_width, dirs[i].ino, dirs[i].name);
 	}
 }
 
